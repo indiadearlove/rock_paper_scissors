e4c329f214d053f7b32794985baba736d6f0cee3
India Dearlove <india_dearlove@yahoo.co.uk>
2015-01-09 15:03:00 +0000

    Can be a new game and write player's name

--- game.rb
+++ game.rb
@@ -6,6 +6,14 @@ class Game < Sinatra::Base
     erb :index
   end
 
+  get '/player' do
+    erb :player
+  end
+
+  get '/player_name' do
+    erb :player_name
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
